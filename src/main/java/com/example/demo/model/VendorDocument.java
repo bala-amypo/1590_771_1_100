d30014cd7df78295f0ccb6bc80f882e55d749cbe
bala-amypo <bala@amypo.in>
2025-12-15 12:43:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/VendorDocument.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class VendorDocument {
+
+    private long id;
+    private Vendor vendor;
+    private DocumentType documentType;
+    private LocalDateTime uploadedAt;
+    private LocalDate expiryDate;
+    private 
+    
+}
