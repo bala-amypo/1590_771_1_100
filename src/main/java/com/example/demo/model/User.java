f302d95955331532aa659ab96c89a69bf9b1eb78
bala-amypo <bala@amypo.in>
2025-12-15 12:13:49 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,10 +1,21 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.context.support.BeanDefinitionDsl.Role;
+
 public class User {
     private long id;
     private String fullName;
     private String email;
     private String password;
-    private String role;
+    private Role role;
+    private LocalDateTime createdAt;
+
+    public class User(){}
+
+    
+
+    
     
 }
