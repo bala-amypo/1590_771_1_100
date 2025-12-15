8c54065b9fe0a09329fd116fe6a285a33c3782ff
bala-amypo <bala@amypo.in>
2025-12-15 12:58:49 +0000

    User commit

--- src/main/java/com/example/demo/model/ComplianceRule.java
+++ src/main/java/com/example/demo/model/ComplianceRule.java
@@ -1,10 +1,21 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
+
 public class ComplianceRule {
 
     private long id;
     private String ruleName;
     private String ruleDescription;
-    private Match
+    private MatchType matchType;
+    private Double threshold;
+    private LocalDateTime createdAt;
+
+    public ComplianceRule(){}
+
+    
+
+
     
 }
