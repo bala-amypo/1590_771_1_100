4a3eacd5ee8c4572a3262e750cf074cab1e0df0c
bala-amypo <bala@amypo.in>
2025-12-15 12:38:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/DocumentType.java
@@ -0,0 +1,77 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+public class DocumentType {
+
+    private long id;
+    private String typeName;
+    private String description;
+    private Boolean required;
+    private Integer weight;
+    private LocalDateTime createdAt;
+
+    public DocumentType(){}
+
+    public DocumentType(String typeName, String description, Boolean required, Integer weight,
+            LocalDateTime createdAt) {
+        this.typeName = typeName;
+        this.description = description;
+        this.required = required;
+        this.weight = weight;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getRequired() {
+        return required;
+    }
+
+    public void setRequired(Boolean required) {
+        this.required = required;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    
+
+
+    
+}
