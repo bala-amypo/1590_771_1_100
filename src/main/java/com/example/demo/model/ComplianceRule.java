62024467e205c0ab7b588e3cb795f29fec7b2e37
bala-amypo <bala@amypo.in>
2025-12-15 13:03:49 +0000

    User commit

--- src/main/java/com/example/demo/model/ComplianceRule.java
+++ src/main/java/com/example/demo/model/ComplianceRule.java
@@ -8,13 +8,72 @@ public class ComplianceRule {
     private long id;
     private String ruleName;
     private String ruleDescription;
-    private MatchType matchType;
+    private String matchType;
     private Double threshold;
     private LocalDateTime createdAt;
 
     public ComplianceRule(){}
 
+    public ComplianceRule(String ruleName, String ruleDescription, String matchType, Double threshold,
+            LocalDateTime createdAt) {
+        this.ruleName = ruleName;
+        this.ruleDescription = ruleDescription;
+        this.matchType = matchType;
+        this.threshold = threshold;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public String getRuleDescription() {
+        return ruleDescription;
+    }
+
+    public void setRuleDescription(String ruleDescription) {
+        this.ruleDescription = ruleDescription;
+    }
+
+    public String getMatchType() {
+        return matchType;
+    }
+
+    public void setMatchType(String matchType) {
+        this.matchType = matchType;
+    }
+
+    public Double getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(Double threshold) {
+        this.threshold = threshold;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     
+
+
 
 
     
---
+++ src/main/java/com/example/demo/model/ComplianceScore.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+public class ComplianceScore {
+
+    private long id;
+    private Vendor vendor;
+    private Double scoreValue;
+    private LocalDateTime lastEvaluated;
+    private String rating
+    
+}
