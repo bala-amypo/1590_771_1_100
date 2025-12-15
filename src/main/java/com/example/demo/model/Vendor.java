5887a6171aa91af511d960208342c28b0b6d423e
bala-amypo <bala@amypo.in>
2025-12-15 12:28:49 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -1,8 +1,78 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
 public class Vendor {
 
     private long id;
-    private 
+    private String vendorName;
+    private String email;
+    private String phone;
+    private String industry;
+    private LocalDateTime createdAt;
+
+    public Vendor(){}
+
+    public Vendor(String vendorName, String email, String phone, String industry, LocalDateTime createdAt) {
+        this.vendorName = vendorName;
+        this.email = email;
+        this.phone = phone;
+        this.industry = industry;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+    
+
+
+
     
 }
