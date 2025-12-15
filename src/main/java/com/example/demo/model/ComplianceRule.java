a1fb080f704d2cbf7cee27b522dbce62b8ed998c
bala-amypo <bala@amypo.in>
2025-12-15 12:48:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ComplianceRule.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public class ComplianceRule {
+
+    private long id;
+    private String ruleName;
+    private String rule;
+    
+}
--- src/main/java/com/example/demo/model/VendorDocument.java
+++ src/main/java/com/example/demo/model/VendorDocument.java
@@ -10,6 +10,67 @@ public class VendorDocument {
     private DocumentType documentType;
     private LocalDateTime uploadedAt;
     private LocalDate expiryDate;
-    private 
+    private Boolean isValid;
+
+    public VendorDocument(){}
+
+    public VendorDocument(Vendor vendor, DocumentType documentType, LocalDateTime uploadedAt, LocalDate expiryDate,
+            Boolean isValid) {
+        this.vendor = vendor;
+        this.documentType = documentType;
+        this.uploadedAt = uploadedAt;
+        this.expiryDate = expiryDate;
+        this.isValid = isValid;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
+
+    public DocumentType getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(DocumentType documentType) {
+        this.documentType = documentType;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Boolean getIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(Boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    
     
 }
