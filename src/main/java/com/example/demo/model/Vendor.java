5e89d0cef04386c4761e46012ed252a32b1a5b6d
bala-amypo <bala@amypo.in>
2025-12-18 09:20:34 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -1,89 +1,89 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-// @Entity
-// @Table(name = "vendors")
-// public class Vendor {
+@Entity
+@Table(name = "vendors")
+public class Vendor {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-//     private long id;
-//     private String vendorName;
-//     private String email;
-//     private String phone;
-//     private String industry;
-//     private LocalDateTime createdAt;
+    private long id;
+    private String vendorName;
+    private String email;
+    private String phone;
+    private String industry;
+    private LocalDateTime createdAt;
 
-//     public Vendor(){}
+    public Vendor(){}
 
-//     public Vendor(String vendorName, String email, String phone, String industry, LocalDateTime createdAt) {
-//         this.vendorName = vendorName;
-//         this.email = email;
-//         this.phone = phone;
-//         this.industry = industry;
-//         this.createdAt = createdAt;
-//     }
+    public Vendor(String vendorName, String email, String phone, String industry, LocalDateTime createdAt) {
+        this.vendorName = vendorName;
+        this.email = email;
+        this.phone = phone;
+        this.industry = industry;
+        this.createdAt = createdAt;
+    }
 
-//     public long getId() {
-//         return id;
-//     }
+    public long getId() {
+        return id;
+    }
 
-//     public void setId(long id) {
-//         this.id = id;
-//     }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-//     public String getVendorName() {
-//         return vendorName;
-//     }
+    public String getVendorName() {
+        return vendorName;
+    }
 
-//     public void setVendorName(String vendorName) {
-//         this.vendorName = vendorName;
-//     }
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
 
-//     public String getEmail() {
-//         return email;
-//     }
+    public String getEmail() {
+        return email;
+    }
 
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-//     public String getPhone() {
-//         return phone;
-//     }
+    public String getPhone() {
+        return phone;
+    }
 
-//     public void setPhone(String phone) {
-//         this.phone = phone;
-//     }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-//     public String getIndustry() {
-//         return industry;
-//     }
+    public String getIndustry() {
+        return industry;
+    }
 
-//     public void setIndustry(String industry) {
-//         this.industry = industry;
-//     }
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
 
-//     public LocalDateTime getCreatedAt() {
-//         return createdAt;
-//     }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
-//     public void setCreatedAt(LocalDateTime createdAt) {
-//         this.createdAt = createdAt;
-//     }
-
-
-    
-
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
 
     
-// }
+
+
+
+    
+}
--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -1,8 +1,8 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import com.example.demo.model.Vendor;
-// import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// public interface VendorRepository extends JpaRepository<Vendor, Long> {
-//     boolean existsByName(String name);
-// }
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    boolean existsByName(String name);
+}
