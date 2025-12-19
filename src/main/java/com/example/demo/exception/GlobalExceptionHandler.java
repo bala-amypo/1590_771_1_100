8651f9dd2d31b314cbffc241f05e63bb53ea9e23
bala-amypo <bala@amypo.in>
2025-12-19 03:50:13 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,20 +1,20 @@
-// package com.example.demo.exception;
+package com.example.demo.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ControllerAdvice;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-// @ControllerAdvice
-// public class GlobalExceptionHandler {
+@ControllerAdvice
+public class GlobalExceptionHandler {
 
-//     @ExceptionHandler(ResourceNotFoundException.class)
-//     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
-//         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-//     }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
-//     @ExceptionHandler(ValidationException.class)
-//     public ResponseEntity<String> handleValidation(ValidationException ex) {
-//         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-//     }
-// }
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidation(ValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
