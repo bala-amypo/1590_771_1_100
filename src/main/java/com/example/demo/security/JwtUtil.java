dc9bf47ba7eb65484292d2215390f2de3c8388c1
bala-amypo <bala@amypo.in>
2025-12-25 14:51:40 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -3,33 +3,23 @@ package com.example.demo.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.JwtParser;
-
-import java.util.Date;
-import java.security.Key;
+import javax.crypto.SecretKey;
 
 public class JwtUtil {
 
-    private final String SECRET = "secret-key-demo-secret-key-demo"; 
-    private final long EXPIRATION = 1000 * 60 * 60; 
-
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(secretKey)
                 .compact();
     }
 
     public String extractUsername(String token) {
-        JwtParser parser = Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build();
-
-        return parser
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
