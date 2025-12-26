0bd2c02e9ca42b7ee9b05729585c4a8528080968
bala-amypo <bala@amypo.in>
2025-12-26 06:33:51 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,19 +1,35 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    public static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
+
         return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://9114.pro604cr.amypo.ai")
-                ));
-        }
+            .info(new Info()
+                .title("Vendor Compliance API")
+                .version("1.0")
+                .description("Vendor Compliance – Document Validator"))
+            .components(new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                    new SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+            .addSecurityItem(
+                new SecurityRequirement().addList(SECURITY_SCHEME_NAME)
+            );
+    }
 }
